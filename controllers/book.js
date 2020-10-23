cd439db39e8d697cf89765b9755e8ffde2c60b49
IvetteCastelblanco <ivetht200130@gmail.com>
2020-10-23 17:50:29 -0500

    Se creo el metodo de actualizar libro y genero

--- controllers/book.js
+++ controllers/book.js
@@ -39,3 +39,41 @@ exports.create = (req, res) =>{
 
 
 }
+
+/**
+ * Metodo para Modificar la informacion
+ * @param {*} req 
+ * @param {*} res 
+ */
+
+exports.update = (req, res) =>{
+    if( Object.entries(req.body).length == 0){
+        return res.status(400).send({
+            message: 'Todos los datos deben estar llenos'
+        })
+
+     }
+
+     const book = {
+        name: req.body.name,
+        author: req.body.author,
+        pageNumber: req.body.pageNumber,
+        publisher: req.body.publisher,
+        publicationDate: req.body.publicationDate,
+        genre: req.body.genre
+     }
+     BookModel.findByIdAndUpdate(req.params.id, book, {new:true} )
+     .then(
+        (bookUpdated) =>{
+            res.send(bookUpdated)
+        }
+      ).catch(
+        (error) =>{
+            return res.status(500).send({
+                message: error.message
+            })
+        }
+    )
+
+
+}
