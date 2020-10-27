b0d0ff18c4af31ef3d7939040f60e429c77fbbf7
IvetteCastelblanco <ivetht200130@gmail.com>
2020-10-27 15:39:57 -0500

    Sec agregaron los metodos de obtener y eliminar a los Libros y usuarios

--- routers/book.js
+++ routers/book.js
@@ -1,6 +1,13 @@
+const user = require('../models/user')
+
 module.exports = (app) =>{
     const book = require ('../controllers/book')
     app.post('/book/create', book.create)
     app.put('/book/update/:id', book.update)
+    app.get('/book/getAll', book.getAll)
+    app.get('/book/getOne/:id', book.getOne)
+    app.delete ('/book/delete/:id', book.deleteOne)
+   
+
 
 }
